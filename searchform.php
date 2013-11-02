97b22063034ca6760bf56e3c569c50695d5518c3
fabrix <fabrix@fabrix.net>
2013-11-02 02:53:22 -0300

    del

--- searchform.php
+++
@@ -1,9 +0,0 @@
-<div class="textwidget"> <!-- classes nativas -->
-<fieldset>
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-<label class="screen-reader-text" for="s">Busca Por:</label>
-<input type="text" value="" name="s" id="s" />
-<input type="submit" id="searchsubmit" value="Buscar" />
-</form>
-</fieldset>
-</div>
--- sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<div id="fluidbarr">';
-dynamic_sidebar( 'sidebar_fluid' );
-echo '</div>';
-?>
-
